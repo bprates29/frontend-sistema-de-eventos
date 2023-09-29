12f058a128e1d74dfbbcfd0d5f1cc44a39f570bd
bernardo_prates <bernardo_prates@sicredi.com.br>
2023-09-29 17:23:26 -0300

    tela inicial

--- src/app/eventos/page.tsx
+++ src/app/eventos/page.tsx
@@ -1,8 +1,14 @@
+import Layout from "@/components/eventos/layout";
+
 export default function Eventos() {
   return (
     <div className={`
-     flex justify-center items-center h-screen`}>
-      <h2>Eventos agora</h2>
+     flex justify-center items-center h-screen
+     bg-gradient-to-bl from-indigo-900 via-indigo-400 to-indigo-900
+     text-white`}>
+      <Layout titulo="Cadastro de eventos">
+        Conteúdo
+      </Layout>
     </div>
   )
 }
