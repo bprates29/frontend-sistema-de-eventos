91ecb78db62bdaa7372116d664f8b527ea94b3c2
bernardo_prates <bernardo_prates@sicredi.com.br>
2023-10-02 11:33:03 -0300

    inicio tabela

--- src/app/eventos/page.tsx
+++ src/app/eventos/page.tsx
@@ -1,13 +1,18 @@
 import Layout from "@/components/eventos/layout";
+import Tabela from "@/components/eventos/tabela";
+import Evento from "@/core/Evento";
 
 export default function Eventos() {
+
+  const eventos = Evento.geraEventosMock()
+
   return (
     <div className={`
      flex justify-center items-center h-screen
      bg-gradient-to-bl from-indigo-900 via-indigo-400 to-indigo-900
      text-white`}>
       <Layout titulo="Cadastro de eventos">
-        Conteúdo
+        <Tabela eventos={eventos}></Tabela>
       </Layout>
     </div>
   )
