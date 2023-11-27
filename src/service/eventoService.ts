3a40647fe353f2a8f84c9f214c91c8ff99df63be
bernardo_prates <bernardo_prates@sicredi.com.br>
2023-11-27 11:36:01 -0300

    Mockando uma chamada no backend

--- src/service/eventoService.ts
+++ src/service/eventoService.ts
@@ -1,46 +1,68 @@
-import axios from 'axios';
 import Evento from '../core/Evento';
 
-interface ApiResponse {
-    content: Evento[];
-}
-
-const BASE_URL = 'https://www.eventos.bprates.com.br/';
+let eventosList: Evento[] = [
+  new Evento(1, "UPF em Dança",
+    "2023-11-27",
+    "Evento de dança",
+    "PREVISTO",
+  ),
+  new Evento(2, "UPF na feitech",
+    "2023-11-27",
+    "Evento de tecnologia",
+    "PREVISTO",
+  )
+]
+let proximoId = eventosList.length + 1;
 
 export const fetchEventos = async (): Promise<Evento[]> => {
   try {
-    const response = await axios.get<ApiResponse>(`${BASE_URL}/eventos`);
-    return response.data.content;
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    return eventosList;
   } catch (error) {
     throw new Error('Erro ao buscar eventos');
   }
 };
 
-export const cadastrarEvento = async (evento: Evento): Promise<Evento> => {
-    try {
-      const response = await axios.post<Evento>(`${BASE_URL}/eventos`, evento);
-      return response.data;
-    } catch (error) {
-      console.error("Erro ao cadastrar evento:", error);
-      throw error;
-    }
-  };
+export const cadastrarEvento = async (novoEvento: Evento): Promise<Evento> => {
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 500));
 
-  export const atualizarEvento = async (evento: Evento): Promise<Evento> => {
-    try {
-      const response = await axios.put<Evento>(`${BASE_URL}/eventos/${evento.id}`, evento);
-      return response.data;
-    } catch (error) {
-      console.error("Erro ao atualizar evento:", error);
-      throw error;
-    }
-  };
+    novoEvento.id = proximoId++;
+    eventosList.push(novoEvento);
 
-  export const excluirEvento = async (id: number): Promise<void> => {
-    try {
-      await axios.delete(`${BASE_URL}/eventos/${id}`);
-    } catch (error) {
-      console.error("Erro ao excluir evento:", error);
-      throw error;
+    // Retorna o evento cadastrado
+    return novoEvento;
+  } catch (error) {
+    console.error("Erro ao cadastrar evento:", error);
+    throw error;
+  }
+};
+
+export const atualizarEvento = async (eventoAtualizado: Evento): Promise<Evento> => {
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    const index = eventosList.findIndex((evento) => evento.id === eventoAtualizado.id);
+
+    if (index !== -1) {
+      eventosList[index] = eventoAtualizado;
+      return eventoAtualizado;
+    } else {
+      throw new Error('Evento não encontrado');
     }
-  };
+  } catch (error) {
+    console.error("Erro ao atualizar evento:", error);
+    throw error;
+  }
+};
+
+export const excluirEvento = async (id: number): Promise<void> => {
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    eventosList = eventosList.filter((evento) => evento.id !== id);
+  } catch (error) {
+    console.error("Erro ao excluir evento:", error);
+    throw error;
+  }
+};
